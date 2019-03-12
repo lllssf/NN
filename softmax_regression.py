65d4fcf650cf147ab413cfdd5982a52986709048
lllssf <lsf0614@163.com>
2019-03-13 03:11:47 +0800

    Update softmax_regression.py

--- softmax_regression.py
+++ softmax_regression.py
@@ -1,29 +1,29 @@
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data/',one_hot=True)
-print(mnist.train.images.shape, mnist.train.labels.shape)
-print(mnist.test.images.shape, mnist.test.labels.shape)
-print(mnist.validation.images.shape, mnist.validation.labels.shape)
+#print(mnist.train.images.shape, mnist.train.labels.shape)
+#print(mnist.test.images.shape, mnist.test.labels.shape)
+#print(mnist.validation.images.shape, mnist.validation.labels.shape)
 
 sess = tf.InteractiveSession() # default session
 x = tf.placeholder(tf.float32,[None,784]) # input
 
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-y_hat = tf.nn.softmax(tf.matmul(x,W)+b)
-y = tf.placeholder(tf.float32,[None,10])
+W = tf.Variable(tf.zeros([784,10])) # weight
+b = tf.Variable(tf.zeros([10]))     # biases
+y_hat = tf.nn.softmax(tf.matmul(x,W)+b)     #hypothesis function
+y = tf.placeholder(tf.float32,[None,10])    # real data
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_hat), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_hat), reduction_indices=[1])) # loss function
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy) # Use Gd to minimize the loss
 
-tf.global_variables_initializer().run()
+tf.global_variables_initializer().run() 
 
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
+for i in range(1000):     # Mini batch:512
+    batch_xs, batch_ys = mnist.train.next_batch(512)
     train_step.run({x: batch_xs, y: batch_ys})
 
 correct_prediction  =tf.equal(tf.argmax(y,1), tf.argmax(y_hat,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) # Accurracy
 print(accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
 
 sess.close()
